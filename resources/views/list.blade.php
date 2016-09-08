4786834acd03c0e04f3f23c48229d5cd68477893
alexa0503 <lori.w@live.cn>
2016-09-08 17:34:57 +0800

    榜单样式

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,4 +1,12 @@
 <table class="myTable" width="100%">
+    <tr id="tableNav">
+      <td width="12%">排名</td>
+      <td width="22%">姓名</td>
+      <td width="16%">财富(亿元)</td>
+      <td width="16%">财富变化</td>
+      <td width="16%">财富来源</td>
+      <td width="12%">居住地</td>
+    </tr>
     @foreach ($list as $key=>$value)
     <tr @if ($value['isUser'] == true){!! 'class="first"'!!}@endif>
       <td>{{$key+1}}<img src="images/@if ($value['scale'] > 0 ){{ 'up.png' }}@elseif ($value['scale'] < 0 ){{ 'down.png' }}@else{{'equal.png'}}@endif" /></td>
